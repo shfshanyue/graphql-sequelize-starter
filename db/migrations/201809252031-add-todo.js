59a3706f1fdf0897d4c3c57b7596fb9d034dceb3
shfshanyue <xianger94@gmail.com>
2018-09-25 21:36:15 +0800

    Add npm start

--- db/migrations/201809252031-add-todo.js
+++ db/migrations/201809252031-add-todo.js
@@ -8,7 +8,7 @@ module.exports = {
         autoIncrement: true
       },
       name: {
-        type: Sequelize.STRING, 
+        type: Sequelize.STRING,
         allowNull: false
       },
       status: {
