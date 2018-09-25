4871cf94de1000b2d5a14027e40c43fd50cdd990
shfshanyue <xianger94@gmail.com>
2018-09-25 20:44:51 +0800

    Add db:migrate

---
+++ db/migrations/201809252031-add-todo.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up (queryInterface, Sequelize) {
+    return queryInterface.createTable('todo', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING, 
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.ENUM('DONE', 'UNDO'),
+        allowNull: false
+      }
+    })
+  },
+
+  down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('todo')
+  }
+}
