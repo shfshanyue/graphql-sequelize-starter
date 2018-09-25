c32ed9a5502d6884c71b7401b309e80dd4e48d1a
shfshanyue <xianger94@gmail.com>
2018-09-25 20:22:23 +0800

    Add db:sync

---
+++ bin/sync.js
@@ -0,0 +1,29 @@
+const SequelizeAuto = require('sequelize-auto')
+const rimraf = require('rimraf')
+const { db } = require('../config')
+
+const SCHEMA_PATH = `${__dirname}/../db/_schemas`
+
+rimraf(SCHEMA_PATH, () => {
+  console.log('delete schema path DONE!')
+})
+
+const auto = new SequelizeAuto(db.database, db.username, '', {
+  dialect: 'postgres',
+  host: db.host,
+  port: db.port,
+  additional: {
+    timestamps: false
+  },
+  spaces: true,
+  indentation: 2,
+  directory: SCHEMA_PATH
+})
+
+auto.run((err) => {
+  if (err) {
+    throw err
+  }
+  console.log('sync database DONE!')
+  process.exit(0)
+})
