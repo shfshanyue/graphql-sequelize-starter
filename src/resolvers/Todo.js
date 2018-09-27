33376c9fd38a4c6fd706346905e4b454325c45fc
shfshanyue <xianger94@gmail.com>
2018-09-27 14:28:24 +0800

    Add Todo:createTodo & Todo:updateTodo

--- src/resolvers/Todo.js
+++ src/resolvers/Todo.js
@@ -3,11 +3,41 @@ const typeDef = `
     id: ID!
     name: String!
     createTime: DateTime!
+    user: User!
+  }
+
+  input TodoCreate {
+    name: String!
+  }
+
+  input TodoUpdate {
+    id: ID!
+    name: String!
+  }
+
+  extend type Mutation {
+    createTodo (todo: TodoCreate!): Todo
+    updateTodo (todo: TodoUpdate!): Boolean
   }
 `
 
 const resolver = {
   Todo: {
+  },
+  Mutation: {
+    createTodo (root, { todo }, { models, user }) {
+      return models.todo.create({
+        ...todo,
+        userId: user.id
+      })
+    },
+    updateTodo (root, { todo }, { models }) {
+      return models.todo.update(todo, {
+        where: {
+          id: todo.id
+        }
+      })
+    }
   }
 }
 
